a8a69b05c035c71749ad93369beafa900d5d6c0f
alex182 <format255@bk.ru>
2015-07-28 17:35:09 +0300

    доработка

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 			
 	$("#icon_menu").click(function() {
-		$(".menu_list").slideToggle("slow");
+		$(".menu_list").slideToggle("normal");
 	});  
 
+	$(window).resize(function ()
+    {
+      $(".menu_list").removeAttr("style");
+   	});          
 
     $(".menu_link").click(function() {
         $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 1000);
 		return false;
-	});                                       
-
+	});    
 });
 
